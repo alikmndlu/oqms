51b79b5fa77a7c3c49791ea0402160dd1cb0cc0e
Ali Kmndlu <alikmndlu1@gmail.com>
2021-11-08 21:20:46 +0330

    init project

---
+++ oqms/src/main/java/com/alikmndlu/oqms/OqmsApplication.java
@@ -0,0 +1,13 @@
+package com.alikmndlu.oqms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OqmsApplication.class, args);
+    }
+
+}
